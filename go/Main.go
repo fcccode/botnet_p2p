760882c1e6aaf01cc7e3c771d157528a5aa869f3
Wojciech Gruszka <wojciech@gruszka.eu>
2018-05-08 03:40:33 +0200

    Moved to gorutines implementation.

--- go/Main.go
+++ go/Main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"log"
+	"syscall"
 )
 
 func main() {
 	fmt.Println("Botnet P2P")
 	nat, _ := checkNAT()
+	terminate := make(chan bool)
 	fmt.Printf("NAT: %t\n", nat)
-	serverRoutine(6666)
+	go exitHandler(terminate)
+	serverRoutine(6666, terminate)
+}
+
+func exitHandler(term chan bool) {
+	signalChannel := make(chan os.Signal, 3)
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	<-signalChannel
+	term<-true
+	log.Println("App terminated!")
+	os.Exit(0)
 }
--- go/Network.go
+++ go/Network.go
@@ -5,114 +5,75 @@ import (
 	"net"
 	"strconv"
 	"log"
-	"net/http"
-	"io/ioutil"
 	"io"
 	"github.com/golang/protobuf/proto"
 )
 
-func getRemoteIP() (string, error) {
-	var remoteIP = "127.0.0.1"
-	response, err := http.Get("https://api.ipify.org")
-	if err != nil {
-		return remoteIP, err
-	}
-	defer response.Body.Close()
-	if response.StatusCode == http.StatusOK {
-		bodyBytes, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			return remoteIP, err
-		}
-		remoteIP = string(bodyBytes)
-	}
-	return remoteIP, nil
-}
-
-func getLocalIPs() ([]string, error) {
-	ips := make([]string, 8)
-	interfaces, err := net.Interfaces()
-	if err != nil {
-		return ips, err
-	}
-	for _, i := range interfaces {
-		addresses, err := i.Addrs()
-		if err != nil {
-			return ips, err
-		}
-		for _, addr := range addresses {
-			var ip net.IP
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			}
-			if ip.To4() != nil {
-				ips = append(ips, ip.To4().String())
-			}
-		}
-	}
-	return ips, nil
-}
-
-func checkNAT() (bool, error) {
-	remoteIP, err := getRemoteIP()
-	if err != nil {
-		return true, err
-	}
-	log.Println("Remote IP:", remoteIP)
-	localIPs, err := getLocalIPs()
-
-	for _, localIP := range localIPs {
-		if localIP == remoteIP{
-			return false, nil
-		}
-	}
-
-	return true, nil
-}
-
-func serverRoutine(port int) {
-	listen, err := net.Listen("tcp", ":"+strconv.Itoa(port))
-	defer listen.Close()
+func serverRoutine(port int, terminate chan bool) {
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+	defer listener.Close()
 	if err != nil {
 		log.Fatalf("Listeninig at port %d failed, %s", port, err)
 		return
 	}
 	log.Printf("Listeninig at port: %d", port)
-	for {
-		conn, err := listen.Accept()
-		if err != nil {
-			log.Fatalln(err)
-			continue
+	clients := make([]chan bool, 8)
+	newConnection := make(chan net.Conn)
+
+	go func(l net.Listener) {
+		for {
+			c, err := l.Accept()
+			if err != nil {
+				log.Fatalln(err)
+			}
+			newConnection<-c
 		}
-		log.Println("New connection at:", conn.RemoteAddr().String())
-		done := make(chan struct{})
-		go clientHandler(conn, done)
+	}(listener)
+
+	for {
+		select {
+		case <-terminate:
+			for _, client := range clients {
+				fmt.Println("xD")
+				client<-true
+			}
+			fmt.Printf("Terminating listener\n")
+			return
+		case conn := <-newConnection:
+			log.Println("New connection at:", conn.RemoteAddr().String())
+			done := make(chan bool)
+			clients = append(clients, done)
+			go clientHandler(conn, done)
+		}
 	}
 }
 
-func clientHandler(conn net.Conn, done chan struct{}) {
+func clientHandler(conn net.Conn, done chan bool) {
 	defer conn.Close()
-	var buffer = make([]byte, 12000) //change max buffer size to match docs
+	buffer := make([]byte, 12000)
+	messageChannel := make(chan Message)
+	go func() {
+		for {
+			message := &Message{}
+			_, err := conn.Read(buffer)
+			if err == io.EOF {
+				return
+			}
+			if err := proto.Unmarshal(buffer, message); err != nil {
+				log.Fatalln("Unable to read message.", err)
+				continue
+			}
+			messageChannel<-*message
+		}
+	}()
+
 	for {
 		select {
 		case <-done:
+			fmt.Printf("Terminating connection with client %s\n", conn.RemoteAddr().String())
 			return
-		default:
-			{
-				message := &Message{}
-				_, err := conn.Read(buffer)
-				if err == io.EOF {
-					return
-				}
-				if err := proto.Unmarshal(buffer, message); err != nil {
-					log.Fatalln("Unable to read message.", err)
-					continue
-				}
-				fmt.Printf("Received message of type: %v\n", message.TYPE.String())
-
-			}
+		case message := <- messageChannel:
+			fmt.Printf("Received message of type: %v\n", message.TYPE.String())
 		}
 	}
 }
